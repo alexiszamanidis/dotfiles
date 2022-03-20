b4ebea5d28beb492602d0a37f68779f64f67b032
Alexis Zamanidis <alexiszamanidis@outlook.com>
2022-03-20 17:46:37 +0200

    delete init.vim, add init.lua and options.lua

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,1 @@
+require "alexzam.options"
---
+++ nvim/.config/nvim/lua/alexzam/options.lua
@@ -0,0 +1,30 @@
+local options = {
+    exrc = true,
+    hlsearch = false,
+    hidden = true,
+    errorbells = false,
+    tabstop = 4,
+    softtabstop = 4,
+    shiftwidth = 4,
+    expandtab = true,
+    smartindent = true,
+    number = true,
+    relativenumber = true,
+    wrap = false,
+    smartcase = true,
+    swapfile = false,
+    backup = false,
+    undodir = "~/.vim/undodir",
+    undofile = true,
+    incsearch = true,
+    scrolloff = 8,
+    signcolumn = "yes"
+}
+
+vim.opt.shortmess:append "c"
+
+for k, v in pairs(options) do
+    vim.opt[k] = v
+end
+
+vim.wo.colorcolumn = "80"
