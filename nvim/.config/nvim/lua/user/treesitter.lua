922d07cc7bc3fb689ab7b21734ae0824102e5ed1
Alexis Zamanidis <alexiszamanidis@outlook.com>
2022-04-26 01:32:04 +0300

    add autopairs

--- nvim/.config/nvim/lua/user/treesitter.lua
+++ nvim/.config/nvim/lua/user/treesitter.lua
@@ -1,11 +1,17 @@
-local configs = require'nvim-treesitter.configs'
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+  return
+end
 
 configs.setup {
-    ensure_installed = "all",
-    highlight = {
-        enable = true,
-    },
-    indent = {
-        enable = false,
-    }
+  ensure_installed = "all",
+  sync_install = false,
+  autopairs = {
+		enable = true,
+	},
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = true,
+  },
+  indent = { enable = true },
 }
