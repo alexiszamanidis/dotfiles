5fcbeae22be94d3eda5e3f56207f9404dd141003
Alexis Zamanidis <alexiszamanidis@outlook.com>
2022-06-11 12:00:43 +0300

    add buffers number navigation

--- nvim/.config/nvim/lua/user/bufferline.lua
+++ nvim/.config/nvim/lua/user/bufferline.lua
@@ -10,5 +10,14 @@ require("bufferline").setup{}
 vim.cmd [[
 nnoremap <silent><ALT-TAB> :BufferLineCycleNext<CR>
 nnoremap <silent><ALT-SHIFT-TAB> :BufferLineCyclePrev<CR>
-nnoremap <silent><ALT-w> :BufferLineGroupClose<CR>
+nnoremap <silent><SHIFT-w> :BufferLineGroupClose<CR>
+nnoremap <silent><ALT-1> <Cmd>BufferLineGoToBuffer 1<CR>
+nnoremap <silent><ALT-2> <Cmd>BufferLineGoToBuffer 2<CR>
+nnoremap <silent><ALT-3> <Cmd>BufferLineGoToBuffer 3<CR>
+nnoremap <silent><ALT-4> <Cmd>BufferLineGoToBuffer 4<CR>
+nnoremap <silent><ALT-5> <Cmd>BufferLineGoToBuffer 5<CR>
+nnoremap <silent><ALT-6> <Cmd>BufferLineGoToBuffer 6<CR>
+nnoremap <silent><ALT-7> <Cmd>BufferLineGoToBuffer 7<CR>
+nnoremap <silent><ALT-8> <Cmd>BufferLineGoToBuffer 8<CR>
+nnoremap <silent><ALT-9> <Cmd>BufferLineGoToBuffer 9<CR>
 ]]
