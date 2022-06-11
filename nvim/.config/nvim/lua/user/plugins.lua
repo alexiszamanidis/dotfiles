d60e50d2739ea1065cec032850fd2b57e8898dc6
Alexis Zamanidis <alexiszamanidis@outlook.com>
2022-06-11 22:25:55 +0300

    add lsp and symneko-lua server

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -63,10 +63,11 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-path"              -- path completions
   use "hrsh7th/cmp-cmdline"           -- cmdline completions
   use "saadparwaiz1/cmp_luasnip"      -- snippet completions
+  use "hrsh7th/cmp-nvim-lsp"
 
-   -- snippets
-   use "L3MON4D3/LuaSnip"             --snippet engine
-   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+  -- Snippets
+  use "L3MON4D3/LuaSnip"              --snippet engine
+  use "rafamadriz/friendly-snippets"  -- a bunch of snippets to use
 
    -- Telescope
   use "nvim-telescope/telescope.nvim"
@@ -78,6 +79,11 @@ return packer.startup(function(use)
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
   }
+
+  -- LSP
+  use "neovim/nvim-lspconfig"            -- enable LSP
+  use "williamboman/nvim-lsp-installer"  -- simple to use language server installer
+  use "tamago324/nlsp-settings.nvim"     -- language server settings defined in json for
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
