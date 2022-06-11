3a73567d199ed59348756a87312b2000d46d6c34
Alexis Zamanidis <alexiszamanidis@outlook.com>
2022-06-12 01:15:58 +0300

    add null-ls

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -85,6 +85,7 @@ return packer.startup(function(use)
   use "williamboman/nvim-lsp-installer"  -- simple to use language server installer
   use "tamago324/nlsp-settings.nvim"     -- language server settings defined in json for
   use "b0o/SchemaStore.nvim"
+  use "jose-elias-alvarez/null-ls.nvim"  -- for formatters and linters
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
