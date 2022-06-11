3debb80c52213a24fd1d90ca8ef0c8e0b7291073
Alexis Zamanidis <alexiszamanidis@outlook.com>
2022-06-12 00:10:13 +0300

    add lsp servers

--- nvim/.config/nvim/lua/user/lsp/lsp-installer.lua
+++ nvim/.config/nvim/lua/user/lsp/lsp-installer.lua
@@ -3,18 +3,68 @@ if not status_ok then
   return
 end
 
-lsp_installer.on_server_ready(function(server)
-  local opts = {
+local servers = {
+  "cssls",
+  "cssmodules_ls",
+  "html",
+  "jsonls",
+  "sumneko_lua",
+  "tsserver",
+  "bashls",
+}
+
+local settings = {
+  ensure_installed = servers,
+  -- automatic_installation = false,
+  ui = {
+    icons = {
+      -- server_installed = "◍",
+      -- server_pending = "◍",
+      -- server_uninstalled = "◍",
+      -- server_installed = "✓",
+      -- server_pending = "➜",
+      -- server_uninstalled = "✗",
+    },
+    keymaps = {
+      toggle_server_expand = "<CR>",
+      install_server = "i",
+      update_server = "u",
+      check_server_version = "c",
+      update_all_servers = "U",
+      check_outdated_servers = "C",
+      uninstall_server = "X",
+    },
+  },
+
+  log_level = vim.log.levels.INFO,
+  -- max_concurrent_installers = 4,
+  -- install_root_dir = path.concat { vim.fn.stdpath "data", "lsp_servers" },
+}
+
+lsp_installer.setup(settings)
+
+local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_status_ok then
+  return
+end
+
+local opts = {}
+
+for _, server in pairs(servers) do
+  opts = {
     on_attach = require("user.lsp.handlers").on_attach,
     capabilities = require("user.lsp.handlers").capabilities,
   }
 
-   if server.name == "sumneko_lua" then
-     local sumneko_opts = require("user.lsp.settings.sumneko_lua")
-     opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-   end
+  if server == "jsonls" then
+    local jsonls_opts = require "user.lsp.settings.jsonls"
+    opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+  end
 
-  -- This setup() function is exactly the same as lspconfig's setup function.
-  -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-  server:setup(opts)
-end)
+  if server == "sumneko_lua" then
+    local sumneko_opts = require "user.lsp.settings.sumneko_lua"
+    opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+  end
+
+  lspconfig[server].setup(opts)
+end
--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -84,6 +84,7 @@ return packer.startup(function(use)
   use "neovim/nvim-lspconfig"            -- enable LSP
   use "williamboman/nvim-lsp-installer"  -- simple to use language server installer
   use "tamago324/nlsp-settings.nvim"     -- language server settings defined in json for
+  use "b0o/SchemaStore.nvim"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
