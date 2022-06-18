76c012f1ced95c704ec6148b11a264dc6d57442b
Alexis Zamanidis <alexiszamanidis@outlook.com>
2022-06-19 00:30:15 +0300

    fix formatting

--- nvim/.config/nvim/lua/user/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/user/lsp/null-ls.lua
@@ -18,17 +18,17 @@ require("null-ls").setup({
     diagnostics.eslint,
     code_actions.eslint,
   },
-    -- you can reuse a shared lspconfig on_attach callback here
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format({ async = true })
-                end,
-            })
-        end
-    end,
+  -- you can reuse a shared lspconfig on_attach callback here
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+                vim.lsp.buf.format({ bufnr = bufnr })
+            end,
+        })
+    end
+  end,
 })
