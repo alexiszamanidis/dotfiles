26201b561da143915dc7f68e90adc62e02476ccc
Alexis Zamanidis <alexiszamanidis@outlook.com>
2022-06-19 18:13:36 +0300

    add nvim option comments

--- nvim/.config/nvim/lua/user/options.lua
+++ nvim/.config/nvim/lua/user/options.lua
@@ -1,27 +1,28 @@
 local options = {
     exrc = true,
-    hlsearch = false,
+    hlsearch = true,                         -- highlight all matches on previous search pattern
     hidden = true,
     errorbells = false,
-    tabstop = 4,
+    tabstop = 4,                             -- insert 2 spaces for a tab
     softtabstop = 4,
-    shiftwidth = 4,
-    expandtab = true,
-    smartindent = true,
-    number = true,
+    shiftwidth = 4,                          -- the number of spaces inserted for each indentation
+    expandtab = true,                        -- convert tabs to spaces
+    number = true,                           -- set numbered lines
     relativenumber = true,
-    wrap = false,
-    smartcase = true,
-    swapfile = false,
-    backup = false,
+    wrap = false,                            -- display lines as one long line
+    smartcase = true,                        -- smart case
+    smartindent = true,                      -- make indenting smarter again
+    swapfile = false,                        -- creates a swapfile
+    backup = false,                          -- creates a backup file
+    clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
     undodir = "~/.vim/undodir",
-    undofile = true,
+    undofile = true,                         -- enable persistent undo
     incsearch = true,
-    scrolloff = 8,
-    signcolumn = "yes",
+    scrolloff = 8,                           -- is one of my fav
+    signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
     cursorcolumn = true,
-    cursorline = true,
-    ignorecase = true
+    cursorline = true,                       -- highlight the current line
+    ignorecase = true                        -- ignore case in search patterns
 }
 
 vim.opt.shortmess:append "c"
