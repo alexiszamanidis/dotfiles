cae520c2bec333df9e6e20a73831f6141d816a62
Alexis Zamanidis <alexiszamanidis@outlook.com>
2022-07-16 22:49:46 +0300

    feat(neovim): update options.lua

--- nvim/.config/nvim/lua/user/options.lua
+++ nvim/.config/nvim/lua/user/options.lua
@@ -8,7 +8,7 @@ local options = {
     shiftwidth = 4,                          -- the number of spaces inserted for each indentation
     expandtab = true,                        -- convert tabs to spaces
     number = true,                           -- set numbered lines
-    relativenumber = true,
+    relativenumber = true,                   -- set relative numbered lines
     wrap = false,                            -- display lines as one long line
     smartcase = true,                        -- smart case
     smartindent = true,                      -- make indenting smarter again
@@ -18,16 +18,22 @@ local options = {
     undodir = "~/.vim/undodir",
     undofile = true,                         -- enable persistent undo
     incsearch = true,
-    scrolloff = 8,                           -- is one of my fav
+    scrolloff = 8,                           -- determine the number of context lines you would like to see above and below the cursor
     signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
     cursorcolumn = true,
     cursorline = true,                       -- highlight the current line
     ignorecase = true,                       -- ignore case in search patterns
-    splitbelow = true,
-    splitright = true
+    splitbelow = true,                       -- force all horizontal splits to go below current window
+    splitright = true,                       -- force all vertical splits to go to the right of current window
+    colorcolumn = "80"
 }
 
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.fillchars = vim.opt.fillchars + 'eob: '
+
 vim.opt.shortmess:append "c"
+
 -- Treesitter folding
 -- vim.opt.foldmethod = "expr"
 -- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
@@ -36,4 +42,5 @@ for k, v in pairs(options) do
     vim.opt[k] = v
 end
 
-vim.wo.colorcolumn = "80"
+vim.cmd "set whichwrap+=<,>,[,],h,l"
+vim.cmd [[set iskeyword+=-]]
