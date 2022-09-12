c19341618e83747f5b77ac39758dc9a6e3f859fd
Alexis Zamanidis <alexiszamanidis@outlook.com>
2022-09-12 22:47:47 +0300

    feat(nvim): hightlight when yanking

--- nvim/.config/nvim/lua/user/init.lua
+++ nvim/.config/nvim/lua/user/init.lua
@@ -3,3 +3,18 @@ require("user.keymaps")
 require("user.colorscheme")
 require("user.plugins")
 require("user.lsp")
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup("HighlightYank", {})
+
+autocmd("TextYankPost", {
+    group = yank_group,
+    pattern = "*",
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = "IncSearch",
+            timeout = 40,
+        })
+    end,
+})
