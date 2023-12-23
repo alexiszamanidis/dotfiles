4100937d6848017364213fe853f8a00ab1293a9a
alexiszamanidis <alexiszamanidis@outlook.com>
2023-12-23 16:38:51 +0200

    feat(nvim): add plugin commit hash

--- nvim/.config/nvim/lua/user/packer.lua
+++ nvim/.config/nvim/lua/user/packer.lua
@@ -44,38 +44,38 @@ return packer.startup(function(use)
     -- My plugins here
 
     -- Have packer manage itself
-    use({ "wbthomason/packer.nvim" })
+    use({ "wbthomason/packer.nvim", commit = "ea0cc3c" })
     -- An implementation of the Popup API from vim in Neovim
-    use({ "nvim-lua/popup.nvim" })
-    -- Useful lua functions used ny lots of plugins
-    use({ "nvim-lua/plenary.nvim" })
-    use({ "windwp/nvim-ts-autotag" })
-    use({ "akinsho/bufferline.nvim" })
-    use({ "nvim-lualine/lualine.nvim" })
-    use({ "kyazdani42/nvim-tree.lua" })
-    use({ "laytan/cloak.nvim" })
+    use({ "nvim-lua/popup.nvim", commit = "b7404d3" })
+    -- Useful lua functions used by lots of plugins
+    use({ "nvim-lua/plenary.nvim", commit = "55d9fe8" })
+    use({ "windwp/nvim-ts-autotag", commit = "8515e48" })
+    use({ "akinsho/bufferline.nvim", commit = "e48ce18" })
+    use({ "nvim-lualine/lualine.nvim", commit = "2248ef2" })
+    use({ "kyazdani42/nvim-tree.lua", commit = "50f30bc" })
+    use({ "laytan/cloak.nvim", commit = "951b163" })
 
     -- UTILITIES
-    use({ "lewis6991/impatient.nvim" })
+    use({ "lewis6991/impatient.nvim", commit = "47302af" })
 
     -- ICONS
-    use({ "kyazdani42/nvim-web-devicons" })
+    use({ "kyazdani42/nvim-web-devicons", commit = "a142590" })
 
     -- COMMENTS
-    use({ "numToStr/Comment.nvim" })
-    use({ "folke/todo-comments.nvim" })
+    use({ "numToStr/Comment.nvim", commit = "0236521" })
+    use({ "folke/todo-comments.nvim", commit = "4a6737a" })
 
     -- UI
-    use({ "folke/which-key.nvim" })
+    use({ "folke/which-key.nvim", commit = "4433e5e" })
 
     -- COLORSCHME
-    use({ "Mofiqul/dracula.nvim" })
+    use({ "Mofiqul/dracula.nvim", commit = "cadf9a1" })
 
     -- TabNine
-    use({ "tzachar/cmp-tabnine", run = "./install.sh" })
+    use({ "tzachar/cmp-tabnine", run = "./install.sh", commit = "b93f82e" })
 
     -- TELESCOPE
-    use({ "nvim-telescope/telescope.nvim" })
+    use({ "nvim-telescope/telescope.nvim", commit = "f336f8c" })
 
     -- TREESITTER
     use({
@@ -84,16 +84,18 @@ return packer.startup(function(use)
             local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
             ts_update()
         end,
+        commit = "732c8cb",
     })
     -- sticky header
-    use({ "nvim-treesitter/nvim-treesitter-context" })
+    use({ "nvim-treesitter/nvim-treesitter-context", commit = "c9f2b42" })
 
     -- GIT
-    use({ "dinhhuy258/git.nvim" })
-    use({ "lewis6991/gitsigns.nvim" })
+    use({ "dinhhuy258/git.nvim", commit = "7416966" })
+    use({ "lewis6991/gitsigns.nvim", commit = "d195f0c" })
     use({
         "sindrets/diffview.nvim",
         requires = "nvim-lua/plenary.nvim",
+        commit = "3dc498c",
     })
 
     -- LSP
@@ -101,34 +103,35 @@ return packer.startup(function(use)
         "VonHeikemen/lsp-zero.nvim",
         requires = {
             -- LSP Support
-            { "neovim/nvim-lspconfig" },
-            { "williamboman/mason.nvim" },
-            { "williamboman/mason-lspconfig.nvim" },
+            { "neovim/nvim-lspconfig",             commit = "a2e84dd" },
+            { "williamboman/mason.nvim",           commit = "41e75af" },
+            { "williamboman/mason-lspconfig.nvim", commit = "9453e3d" },
 
             -- Autocompletion
-            { "hrsh7th/nvim-cmp" },
-            { "hrsh7th/cmp-buffer" },
-            { "hrsh7th/cmp-path" },
-            { "saadparwaiz1/cmp_luasnip" },
-            { "hrsh7th/cmp-nvim-lsp" },
-            { "hrsh7th/cmp-nvim-lua" },
+            { "hrsh7th/nvim-cmp",                  commit = "538e37b" },
+            { "hrsh7th/cmp-buffer",                commit = "3022dbc" },
+            { "hrsh7th/cmp-path",                  commit = "91ff86c" },
+            { "saadparwaiz1/cmp_luasnip",          commit = "05a9ab2" },
+            { "hrsh7th/cmp-nvim-lsp",              commit = "5af77f5" },
+            { "hrsh7th/cmp-nvim-lua",              commit = "f12408b" },
 
             -- Snippets
-            { "L3MON4D3/LuaSnip" },
-            { "rafamadriz/friendly-snippets" },
+            { "L3MON4D3/LuaSnip",                  commit = "57c9f5c" },
+            { "rafamadriz/friendly-snippets",      commit = "53d3df2" },
         },
+        commit = "b904471",
     })
 
     -- formatters and linters
-    use({ "jose-elias-alvarez/null-ls.nvim" })
+    use({ "jose-elias-alvarez/null-ls.nvim", commit = "0010ea9" })
 
     -- Java
-    use({ "mfussenegger/nvim-jdtls" })
+    use({ "mfussenegger/nvim-jdtls", commit = "66b5ace" })
 
     -- DAP
-    use({ "mfussenegger/nvim-dap" })
-    use({ "rcarriga/nvim-dap-ui" })
-    use({ "ravenxrz/DAPInstall.nvim" })
+    use({ "mfussenegger/nvim-dap", commit = "f0dca67" })
+    use({ "rcarriga/nvim-dap-ui", commit = "34160a7" })
+    use({ "ravenxrz/DAPInstall.nvim", commit = "8798b4c" })
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
