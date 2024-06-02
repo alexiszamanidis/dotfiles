56f6fba9424f7680da6319ac1dd66d43f88cd0a1
alexiszamanidis <alexiszamanidis@outlook.com>
2024-06-02 22:36:55 +0300

    feat(lazy): add autotags, cloak, codeium, comments, dap, git, illuminate, impatient

---
+++ nvim/.config/nvim/lua/plugins/codeium.lua
@@ -0,0 +1,20 @@
+return {
+    {
+        "Exafunction/codeium.vim",
+        event = "InsertEnter",
+        config = function()
+            vim.keymap.set("i", "<C-g>", function()
+                return vim.fn["codeium#Accept"]()
+            end, { expr = true, silent = true })
+            vim.keymap.set("i", "<C-j>", function()
+                return vim.fn["codeium#CycleCompletions"](1)
+            end, { expr = true, silent = true })
+            vim.keymap.set("i", "<C-k>", function()
+                return vim.fn["codeium#CycleCompletions"](-1)
+            end, { expr = true, silent = true })
+            vim.keymap.set("i", "<C-x>", function()
+                return vim.fn["codeium#Clear"]()
+            end, { expr = true, silent = true })
+        end,
+    },
+}
