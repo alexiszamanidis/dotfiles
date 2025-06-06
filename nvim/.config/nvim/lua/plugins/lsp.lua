a3d6863f4243d5cce1499bf7a55966c29a561b8b
alexiszamanidis <alexiszamanidis@outlook.com>
2025-06-06 20:56:20 +0300

    feat(nvim): add shellcheck and stylua

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -32,6 +32,7 @@ return {
             ensure_installed = {
                 "ansible_lint",
                 "yamllint",
+                "shellcheck",
             },
         })
         -- Install all Formatters
