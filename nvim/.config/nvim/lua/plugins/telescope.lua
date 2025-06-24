e34cd88a97273937d2f90299c7bdf8c9f63a0c88
alexiszamanidis <alexiszamanidis@outlook.com>
2025-06-24 09:36:47 +0300

    feat(nvim): add telescope keymaps, remove cloak

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -44,6 +44,11 @@ return {
             })
 
             local builtin = require("telescope.builtin")
+            -- I always execute some of the following commands when I open nvim
+            -- and which-key is a bit slow. That's why I define these keymaps twice
+            vim.keymap.set("n", "<C-p>", builtin.find_files)
+            vim.keymap.set("n", "<leader>ff", builtin.find_files)
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep)
 
             local mappings = {
                 {
